14cdc1e97b57b1599cd9257ddb041948af6b0d54
souts jjw <souts.jjw@gmail.com>
2022-11-05 17:34:40 +0800

    Code First

---
+++ SampleCodeFirst/SampleCodeFirst/Models/Article.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleCodeFirst.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string? Content { get; set; }
+
+        public int Read { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public DateTime PostTime { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ SampleCodeFirst/SampleCodeFirst/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SampleCodeFirst.Models
+{
+    public class User
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string UserName { get; set; }
+
+        public virtual ICollection<Article> Articles { get; set; }
+    }
+}
