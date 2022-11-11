0bca192405322a59f79e0c2b38597904219ff7c7
souts jjw <souts.jjw@gmail.com>
2022-11-11 13:37:07 +0800

    跨網站偽造要求(XSRF/CSRF)

--- SampleAPI/Program.cs
+++ SampleAPI/Program.cs
@@ -34,7 +34,6 @@ try
 
     var builder = WebApplication.CreateBuilder(args);
 
-    
     builder.Services.AddAntiforgery(options => options.HeaderName = "X-XSRF-TOKEN");
     builder.Services.AddAuthentication().AddCookie(c => c.Cookie.HttpOnly = true);
 
@@ -197,6 +196,23 @@ try
     {
         return "hello ep2";
     }).RequireCors("CustomPolicy1");
+
+    var antiforgery = app.Services.GetRequiredService<IAntiforgery>();
+
+    app.Use((context, next) =>
+    {
+        var requestPath = context.Request.Path.Value;
+
+        if (string.Equals(requestPath, "/", StringComparison.OrdinalIgnoreCase)
+            || string.Equals(requestPath, "/index.html", StringComparison.OrdinalIgnoreCase))
+        {
+            var tokenSet = antiforgery.GetAndStoreTokens(context);
+            context.Response.Cookies.Append("XSRF-TOKEN", tokenSet.RequestToken!,
+                new CookieOptions { HttpOnly = false });
+        }
+
+        return next(context);
+    });
 
     app.Run();
 }
